d164bdff0fcc44f497942a071f3bee0fbd021104
Velshh <pkot2003@gmail.com>
2024-08-23 22:46:19 +0200

    Initial commit

---
+++ src/main/java/me/mmtr/noted/NotedApplication.java
@@ -0,0 +1,14 @@
+package me.mmtr.noted;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotedApplication.class, args);
+    }
+
+}
