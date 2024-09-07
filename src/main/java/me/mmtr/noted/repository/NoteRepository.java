0c9b3741e214bef53f547dee0fdc600df8e48a05
Velshh <pkot2003@gmail.com>
2024-09-07 16:35:02 +0200

    remove unused NoteRepository.java

--- src/main/java/me/mmtr/noted/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package me.mmtr.noted.repository;
-
-import me.mmtr.noted.data.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
